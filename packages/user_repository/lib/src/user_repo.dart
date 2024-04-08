e08a1e60144b8ab8dcc8c0847790e90575a8e438
VlCTQR <victor.zwaga@gmail.com>
2024-04-08 12:18:02 +0100

    Cloud Firestore User Repository Setup

--- packages/user_repository/lib/src/user_repo.dart
+++ packages/user_repository/lib/src/user_repo.dart
@@ -8,4 +8,10 @@ abstract class UserRepository {
   Future<MyUser> signUp(MyUser myUser, String password);
 
   Future<void> resetPassword(String email);
+
+  // setUserData
+  Future<void> setUserData(MyUser user);
+
+  // getMyUser
+  Future<MyUser> getMyUser(String myUserId);
 }
